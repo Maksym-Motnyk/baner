7070cdf6f094ca67e6ca57e56468fbad3f325819
Maksym Motnyk <maksummotnuk@gmail.com>
2025-05-16 21:22:59 +0300

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const wrap = document.querySelector(".container");
+const button = document.querySelector(".close-btn");
+
+button.addEventListener("click", () => {
+  wrap.classList.add("hide");
+});
