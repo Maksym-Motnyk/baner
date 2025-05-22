0a7a86c77e6db621e50caa3175772c82563eb9b4
Maksym Motnyk <maksummotnuk@gmail.com>
2025-05-22 17:59:25 +0300

    delete coment

--- script.js
+++ script.js
@@ -5,18 +5,3 @@ button.addEventListener("click", () => {
   wrap.classList.add("hide");
 });
 
-// const messages = ["BUY NOW", "SUPER SALE", "NEW ARRIVAL", "BEST SELLER"];
-// let index = 0;
-// const bannerText = document.querySelector(".banner-wrap");
-
-// function cycleText() {
-//   bannerText.classList.remove("show");
-//   setTimeout(() => {
-//     bannerText.textContent = messages[index];
-//     bannerText.classList.add("show");
-//     index = (index + 1) % messages.length;
-//   }, 500);
-// }
-
-// setInterval(cycleText, 3000);
-// bannerText.classList.add("show"); // initial
